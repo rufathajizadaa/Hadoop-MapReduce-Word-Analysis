bc201a48c60545e5e493553cc4142b9595d0eb55
rufathajizadaa <rufeth04@gmail.com>
2025-10-24 23:11:46 +0400

    reducer (initial version) ready

---
+++ hadoop_reducer.py
@@ -0,0 +1,18 @@
+import sys
+
+curr_word = None
+curr_cnt = 0
+
+for ln in sys.stdin:
+    word, cnt = ln.strip().split("\t")
+    cnt = int(cnt)
+    if curr_word == word:
+        curr_cnt += cnt
+    else:
+        if curr_word:
+            print(f"{curr_word}\t{curr_cnt}")
+        curr_word = word
+        curr_cnt = cnt
+
+if curr_word:
+    print(f"{curr_word}\t{curr_cnt}")
